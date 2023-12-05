2b58cbc97a6d78c303e5c06dcdfb52a614ca67c3
Lars de Kooter <de.kooter.lars@gmail.com>
2023-12-05 16:22:05 +0100

    printing update

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def train():
             network.nGames+=1
             network.trainLongMemory()
 
-            print("game", network.nGames, "won", won, "x", x, "y", y, '%', round(aiSteps / (aiSteps+randomSteps) * 100, 2), "Total steps", network.decayStep, "time", round(gameTime, 2), "s", "\n", "0", round(moves[0] / totalMoves * 100.0, 2), "1", round(moves[1] / totalMoves * 100.0, 2), "2", round(moves[2] / totalMoves * 100.0, 2), "3", round(moves[3] / totalMoves * 100.0, 2))
+            print("game", network.nGames, "won", won, "x", x, "y", y, '%', round(aiSteps / (aiSteps+randomSteps) * 100, 2), "Total steps", network.decayStep, "time", round(gameTime, 2), "s", "\n", "0", round(moves[0] / totalMoves * 100.0, 2), "| 1", round(moves[1] / totalMoves * 100.0, 2), "| 2", round(moves[2] / totalMoves * 100.0, 2), "| 3", round(moves[3] / totalMoves * 100.0, 2))
             if won or network.nGames % 100 == 0:
                 network.model.save()
             game.gameTime = time()
