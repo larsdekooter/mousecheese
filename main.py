b4fad6648c624d36f877abaa010d5ee111e1bf34
larsdekooter <102305474+larsdekooter@users.noreply.github.com>
2023-12-07 12:03:52 +0100

    Part 2

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def train():
     network = Network()
     game = Game()
     while network.nGames < testLength + 100:
+        epsilon = data.minEpsilon + (data.maxEpsilon - data.minEpsilon) * np.exp(-data.decayRate * network.decayStep)
         state = network.getState(game)
         move = network.getAction(state)
 
@@ -46,7 +47,7 @@ def train():
             network.nGames+=1
             network.trainLongMemory()
 
-            print("game", network.nGames, "won", won, "x", x, "y", y, '%', round(aiSteps / (aiSteps+randomSteps) * 100, 2), "Total steps", network.decayStep, "time", round(gameTime, 2), "s", "\n", "0", round(moves[0] / totalMoves * 100.0, 2), "| 1", round(moves[1] / totalMoves * 100.0, 2), "| 2", round(moves[2] / totalMoves * 100.0, 2), "| 3", round(moves[3] / totalMoves * 100.0, 2), "epsilon", )
+            print("game", network.nGames, "won", won, "x", x, "y", y, '%', round(aiSteps / (aiSteps+randomSteps) * 100, 2), "Total steps", network.decayStep, "time", round(gameTime, 2), "s", "\n", "0", round(moves[0] / totalMoves * 100.0, 2), "| 1", round(moves[1] / totalMoves * 100.0, 2), "| 2", round(moves[2] / totalMoves * 100.0, 2), "| 3", round(moves[3] / totalMoves * 100.0, 2), "epsilon", round(epsilon, 2)
             if won or network.nGames % 100 == 0:
                 network.model.save()
             game.gameTime = time()
