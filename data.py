af7da30ef256fdf1cc2e4418b910d87347c9e925
Lars de Kooter <de.kooter.lars@gmail.com>
2023-12-07 18:42:19 +0100

    parameter changes

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 catPositions = [(100, 100), (200, 100), (300, 100), (500, 100), (600, 100), (700, 100), (0, 200), (500, 200), (100, 300), (300, 300), (400, 300), (500, 300), (400, 400), (400, 500), (400, 600), (600, 600), (600, 700)]
 cooldown = 0
-cheeseReward = 1000
+cheeseReward = 1000000
 rewardNerf = 5
 gamma = 0.9
 hiddenSize = 64  # Increased hidden layer size for more capacity
@@ -11,7 +11,7 @@ decayRate = 0.00001
 batchSize = 64  # Adjusted batch size for efficiency
 random = 200
 testLength = 6000
-invalidMovePunishment = -100
+invalidMovePunishment = -10000
 
 def getDistanceReward(distance):
     return 1.02 ** -distance  # Slightly increased reward decay
